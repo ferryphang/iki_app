321d61d5028af56d42321671297d201a9fb160d1
YOUR NAME <YOUR@EMAIL.com>
2018-03-15 13:11:43 +0700

    Penambahan code nama

--- iki.rb
+++ iki.rb
@@ -2,6 +2,13 @@ class Iki
   attr_accessor :name, :age, :gender
 
   def initialize(name, age, gender)
+  	@name = name 
+  	@age = age 
+  	@gender = gender 
+  end
 
+  def namasaya
+  	namasaya = @name
+  	"Nama saya adalah " +namasaya 
   end
 end
