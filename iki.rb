e72325e9874b90dd1e48eabdbbbb4feb7a0485ad
ferry pangestu <ferry.pangestu@ymail.com>
2018-03-15 13:13:24 +0700

    Adding age saya

--- iki.rb
+++ iki.rb
@@ -4,4 +4,23 @@ class Iki
   def initialize(name, age, gender)
 
   end
+
+
+
+
+
+  def age_saya
+    case age
+    when 1..5
+      "Anak Balita"
+    when 6..10
+      "Kanak kanak"
+    when 11..19
+      "Remaja"
+    when 20..30
+      "Dewasa"
+    else
+      "Tuhan"
+    end
+  end
 end
